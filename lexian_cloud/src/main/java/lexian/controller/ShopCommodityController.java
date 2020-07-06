01e69c3cf8cf6daabe3913d23ab424d14d2ff8ba
xiny0919 <1052190271@qq.com>
2020-07-07 00:04:41 +0800

    分页功能更新

--- lexian_cloud/src/main/java/lexian/controller/ShopCommodityController.java
+++ lexian_cloud/src/main/java/lexian/controller/ShopCommodityController.java
@@ -1,9 +1,13 @@
 package lexian.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import lexian.entity.Commodity;
 import lexian.service.CommodityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +22,13 @@ public class ShopCommodityController {
     }
 
     @RequestMapping("/api/shop/goodsInfo")
-    public List<Commodity> getAllShopCommodity(){
-        return commodityService.getAllShopCommodity();
+    public PageInfo<Commodity> getAllShopCommodity(@RequestParam("pageNo") Integer pageNo){
+        if(pageNo==null)
+            pageNo=1;
+        PageHelper.startPage(pageNo,10);
+        List<Commodity> resultList = commodityService.getAllShopCommodity();
+        PageInfo<Commodity> result = new PageInfo<>(resultList);
+        return result;
     }
 
 }
