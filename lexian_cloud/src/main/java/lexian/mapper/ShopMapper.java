4a0bb3e04b45b91e02e5d787d700b08a2a8c3fcc
Yang_jiayu <984561160@qq.com>
2020-07-08 00:08:26 +0800

    店铺管理编辑模块前后端完成

--- lexian_cloud/src/main/java/lexian/mapper/ShopMapper.java
+++ lexian_cloud/src/main/java/lexian/mapper/ShopMapper.java
@@ -6,25 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
- @Mapper
+@Mapper
  @Repository
 public interface ShopMapper {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(Shop record);
-
-    int insertSelective(Shop record);
-
-    Shop selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(Shop record);
-
-    int updateByPrimaryKey(Shop record);
-
     List<Shop> getAllShop();
 
     int getMaxShopId();
 
-//    List<Shop> updateShop(Shop record);
+    int updateShop(Map<String,Object> map);
 }
--- lexian_cloud/src/main/java/lexian/service/ShopService.java
+++ lexian_cloud/src/main/java/lexian/service/ShopService.java
@@ -3,11 +3,11 @@ package lexian.service;
 import lexian.entity.Shop;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ShopService {
     List<Shop> getAllShop();
 
     int getMaxShopId();
 
-//    List<Shop> updateShop(Shop record);
-}
+    boolean updateShop(Map<String,Object> map);}
