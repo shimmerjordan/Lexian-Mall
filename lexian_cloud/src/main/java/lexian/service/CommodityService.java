b2308c3edea02cb642f864324df09b583dcd87af
xiny0919 <1052190271@qq.com>
2020-07-08 13:37:27 +0800

    后台添加新的商品页面+后端

--- lexian_cloud/src/main/java/lexian/service/CommodityService.java
+++ lexian_cloud/src/main/java/lexian/service/CommodityService.java
@@ -6,18 +6,8 @@ import java.util.Map;
 import lexian.entity.Commodity;
 
 public interface CommodityService {
-<<<<<<< HEAD
     List<Commodity> getAllShopCommodity();
-    boolean updateGood(Map<String,Object> map);
     boolean deleteGood(Map<String,Object> map);
-=======
-
-	List<Commodity> getAllShopCommodity();
-
 	boolean updateGood(Map<String, Object> map);
-
 	List<Commodity> listCommodityByCommodityId(String commodityId);
-
-	boolean deleteGood(Integer id);
->>>>>>> 96370e8061cf29b7a93dd2bcba624e85caa4816d
 }
