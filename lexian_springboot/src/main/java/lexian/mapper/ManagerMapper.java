5689e9f11395555b090bbe084a6092174f13575d
zdx.Alexzero <dexin_zeng@qq.com>
2020-07-18 18:11:37 +0800

    完成了order，manager对应的service，mapper的注释

--- lexian_springboot/src/main/java/lexian/mapper/ManagerMapper.java
+++ lexian_springboot/src/main/java/lexian/mapper/ManagerMapper.java
@@ -11,11 +11,31 @@ import java.util.Map;
 @Mapper
 @Repository
 public interface ManagerMapper {
+    /**
+     * 获取当前类型的系统管理员列表
+     * @param i
+     * @return
+     */
     List<Manager> getRoleManagers(int i);
 
+    /**
+     * 增加一名管理员
+     * @param m 新增管理员信息
+     * @return
+     */
     int addManager(Map<String,Object> m);
 
+    /**
+     * 更新管理员信息
+     * @param m
+     * @return
+     */
     int updateManager(Map<String,Object> m);
 
+    /**
+     * 删除一条管理员记录
+     * @param index
+     * @return
+     */
     int deleteManager(int index);
 }
