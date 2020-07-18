1d775ae1c491a9c3e273f913dd9b1e3459cdb80e
Yang_jiayu <984561160@qq.com>
2020-07-18 19:47:34 +0800

    不好意思。。

---
+++ lexian_admin/vue-element-admin-master/src/views/permission-manage/personnel-manage/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-container">
+    <div style="margin:0 0 5px 20px;color: #1890FF;">
+      点击显示角色对应成员
+    </div>
+    <fixed-thead />
+  </div>
+</template>
+
+<script>
+import FixedThead from './components/FixedThead'
+
+export default {
+  name: 'DynamicTable',
+  components: { FixedThead }
+}
+</script>
